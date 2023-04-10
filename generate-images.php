5e891541c82fbe23ebad7d4335929a30ec3c426f
Stefan Bürk <stefan@buerk.tech>
2023-04-10 15:05:28 +0200

    [TASK] Switch to generated images extended from typo3 images
    This change
    
    * ... removed the original, manual provided demonstration
      image from the repository
    * ... adds a generator to generate php images based on the
      typo3/core-testing-phpXY images along with more GitHub
      action workflows for auto creating them if new versions
      are added.
    * ... adding a `pull_request` workflow to verify builds
      before merging them.
    * ... adding actual image definitions for existing typo3
      core-testing-phpXY images
    * ... slightly enhance `README.md`
    * ... auto update image README.md on DockerHub based on
      repository README.md (this may be changed in the future
      to a template based one with auto regeneratong before
      push )

---
+++ generate-images.php
@@ -0,0 +1,107 @@
+#!/usr/bin/env php
+<?php
+$path = realpath(__DIR__);
+chdir($path);
+$imagesFile = $path . '/images.json';
+$images = json_decode(file_get_contents($imagesFile), true, JSON_THROW_ON_ERROR);
+
+function ensureImageDockerFile(
+    string $imageFolder,
+    string $imageName,
+    string $templateFile,
+    string $dockerFileName,
+): ?string {
+    if (!is_file($templateFile)) {
+        return null;
+    }
+    $replaces = [
+        '%%FROM_IMAGE%%' => 'typo3/' . $imageName,
+    ];
+    $content = file_get_contents($templateFile);
+    $content = str_replace(
+        array_keys($replaces),
+        array_values($replaces),
+        $content
+    );
+    $old = file_exists($dockerFileName) ? file_get_contents($dockerFileName) : '';
+    file_put_contents($dockerFileName, $content);
+    $result = $old === $content ? 'unchanged' : ($old === '' ? 'new' : 'changed');
+    echo " >> Generated \"$dockerFileName\" from \"$templateFile\" [$result]" . PHP_EOL;
+    return $result;
+}
+
+function ensureTYPO3Repository(string $basePath, array &$images): void
+{
+    $currentDirectory = getcwd();
+    $repoParentPath = $basePath . '/tmp';
+    $repoBaseName = 'testing-infrastructure';
+    $repoPath = $repoParentPath . '/' . $repoBaseName;
+    if (!is_file($repoPath . '/.git/config')) {
+        if (is_dir($repoPath)) {
+            `rm -Rf $repoPath`;
+        }
+        if (!is_dir($repoParentPath)) {
+            @mkdir($repoParentPath, 0777, true);
+        }
+        chdir($repoParentPath);
+        `git clone https://git.typo3.org/typo3/CI/testing-infrastructure.git/ ./$repoBaseName`;
+    }
+
+    chdir($repoPath);
+    `git checkout main`;
+    `git pull --rebase`;
+    chdir($repoPath . '/docker-images');
+    $folders = glob(
+        "core-testing-php??",
+        GLOB_ONLYDIR | GLOB_BRACE
+    );
+    chdir($currentDirectory);
+
+    foreach ($folders as $baseImageName) {
+        if (!is_dir($basePath . '/' . $baseImageName)) {
+            @mkdir($basePath . '/' . $baseImageName);
+        }
+
+        $dockerFileName = $basePath . '/' . $baseImageName . '/Dockerfile';
+        $templateFile = $basePath . '/templates/Dockerfile-core-testing-phpXY';
+        if (file_exists($basePath . '/templates/Dockerfile-' . $baseImageName)) {
+            $templateFile = $basePath . '/templates/Dockerfile-' . $baseImageName;
+        }
+        $changed = ensureImageDockerFile(
+            $basePath . '/' . $baseImageName,
+            $baseImageName,
+            $templateFile,
+            $dockerFileName,
+        );
+        if ($changed !== 'unchanged' && file_exists($dockerFileName)) {
+            echo " >> $changed image configuration for $baseImageName" . PHP_EOL;
+            $image = $images[$baseImageName] ?? [
+                "context" => $baseImageName,
+                "docker-image-basename" => "sbuerk/$baseImageName",
+                "docker-image-name" => "sbuerk/demo-$baseImageName",
+                "full" => "1.0.0",
+                "gh-image-name" => "ghcr.io/sbuerk/demo-$baseImageName",
+                "lookup-name" => $baseImageName,
+                "major" => 1,
+                "minor" => 0,
+                "patchlevel" => 0,
+                "platforms" => "linux/amd64,linux/arm64",
+                "short" => "1.0",
+            ];
+            if ($changed === "changed") {
+                $image['patchlevel']++;
+                $image['full'] = implode('.', [$image['major'], $image['minor'], $image['patchlevel']]);
+                $image['short'] = implode('.', [$image['major'], $image['minor']]);
+            }
+            ksort($image);
+            $images[$baseImageName] = $image;
+            ksort($images);
+        }
+    }
+}
+
+ensureTYPO3Repository($path,$images);
+ksort($images);
+$written = file_put_contents($imagesFile, \json_encode($images, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+echo " >> Updates $imagesFile - written $written bytes" . PHP_EOL;
+echo PHP_EOL;
